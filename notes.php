cdda467da5efb6b60fd88785eea88bce5abe6548
Ayoub TAdili <ayoubtadili139@gmail.com>
2026-05-03 20:04:35 +0100

    determiner la mention

--- notes.php
+++ notes.php
@@ -8,3 +8,17 @@ function calculerMoyenne($notes)
 
     return round(array_sum($notes) / count($notes), 2);
 }
+
+function determinerMention($moyenne){
+    if ($moyenne >= 16) {
+        return "Très Bien";
+    } elseif ($moyenne >= 14) {
+        return "Bien";
+    } elseif ($moyenne >= 12) {
+        return "Assez Bien";
+    } elseif ($moyenne >= 10) {
+        return "Passable";
+    } else {
+        return "Insuffisant";
+    }
+}
