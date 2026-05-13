bd90bee08d3d85c594f515119ce4e2f1a99d0a55
Ayoub Tadili <ayoubtadili139@gmail.com>
2026-05-13 14:12:32 +0100

    fonction 6

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
 require_once 'notes.php'; // ta bibloitheque de fonctions
-$etudiants = [
-    ["prenom" => "Yassine", "nom" => "Elbouari", "notes" => [14, 12, 9, 15, 11]],
-    ["prenom" => "Salma", "nom" => "Chraibi", "notes" => [8, 6, 5, 7, 9]],
-    ["prenom" => "Mehdi", "nom" => "Tazi", "notes" => [17, 18, 15, 16, 19]],
-    ["prenom" => "Nour", "nom" => "Idrissi", "notes" => [10, 10, 10, 10, 10]],
-    ["prenom" => "Hamza", "nom" => "Benkiran", "notes" => [12, 9, 14, 8, 11]],
-    ["prenom" => "Lina", "nom" => "Fassi", "notes" => [6, 4, 7, 3, 5]],
-    ["prenom" => "Omar", "nom" => "Sekkat", "notes" => [13, 15, 12, 14, 16]],
-];
-$matiers = ["Maths", "Physique", "Informatique", "Anglais", "Français"];
+$etudiants = array(
+    array("prenom" => "Yassine", "nom" => "Elbouari", "notes" => array(14, 12, 9, 15, 11)),
+    array("prenom" => "Salma", "nom" => "Chraibi", "notes" => array(8, 6, 5, 7, 9)),
+    array("prenom" => "Mehdi", "nom" => "Tazi", "notes" => array(17, 18, 15, 16, 19)),
+    array("prenom" => "Nour", "nom" => "Idrissi", "notes" => array(10, 10, 10, 10, 10)),
+    array("prenom" => "Hamza", "nom" => "Benkiran", "notes" => array(12, 9, 14, 8, 11)),
+    array("prenom" => "Lina", "nom" => "Fassi", "notes" => array(6, 4, 7, 3, 5)),
+    array("prenom" => "Omar", "nom" => "Sekkat", "notes" => array(13, 15, 12, 14, 16)),
+);
+$matiers = array("Maths", "Physique", "Informatique", "Anglais", "Français");
 $seuil_admission = 10;
+$statistiques = calculerStatistiquesPromotion($etudiants, $seuil_admission);
+foreach ($statistiques as $k => $v) {
+    echo "$k : $v \n";
+}
--- notes.php
+++ notes.php
@@ -2,6 +2,7 @@
 
 function calculerMoyenne($notes)
 {
+
     if (count($notes) === 0) {
         return 0;
     }
@@ -33,5 +34,103 @@ function estAdmis($moyenne, $seuil)
 }
 function formaterNomComplet($prenom, $nom)
 {
-    return ucfirst($prenom) . "" . strtoupper($nom) . "";
+    return ucfirst(strtolower($prenom)) . " " . strtoupper($nom) . "";
+}
+function genererReleve($etudiant, $matiere, $seuil)
+{
+    $longueure_ligne = 40;
+    $longueure_intitule = 14;
+    $relevet = "";
+
+    $nom_complet = formaterNomComplet($etudiant['prenom'], $etudiant['nom']);
+
+    $relevet .= str_repeat("=", $longueure_ligne) . "\n";
+    $relevet .= "Releve de notes -- " . $nom_complet . "\n";
+    $relevet .= str_repeat("=", $longueure_ligne) . "\n";
+
+    foreach ($matiere as $nom_matiere => $note_obtenue) {
+        $nom_matiere = ucfirst(strtolower($nom_matiere));
+        $relevet .= str_pad($nom_matiere, $longueure_intitule) . ": " . $note_obtenue . "/20\n";
+    }
+
+    $moyenne = calculerMoyenne(array_values($matiere));
+    $mention = determinerMention($moyenne);
+    $resultat = estAdmis($moyenne, $seuil) ? "ADMIS" : "NON ADMIS";
+
+    $relevet .= str_repeat("-", $longueure_ligne) . "\n";
+    $relevet .= str_pad("Moyenne", $longueure_intitule) . ": " . number_format($moyenne, 2) . "/20\n";
+    $relevet .= str_pad("Mention", $longueure_intitule) . ": " . $mention . "\n";
+    $relevet .= str_pad("Resultat", $longueure_intitule) . ": " . $resultat . "\n";
+    $relevet .= str_repeat("=", $longueure_ligne) . "\n";
+
+    return $relevet;
+
+
+}
+
+/**
+ * $etudiant = array (
+ * array(
+ * "prenom" => "Yassine",
+ * "nom" => "Elbouari",
+ * "notes" => [14,12,9,15,11]
+ * ),
+ * ....
+ * )
+ * $seuil : float
+ * 
+ * 
+ * retourner:
+ * array(
+ * "moyenne_promo" : float,
+ * "meilleur       : str,
+ * "moin_bon"      : str,
+ * "nb_admis"      : int,
+ * "nb_ajournres"  : int,
+ * "taux_reussite" :float
+ *  )
+ */
+
+function calculerStatistiquesPromotion($etudiants, $seuil)
+{
+    $taille = count($etudiants);
+    $statistiques = array(
+
+    );
+    //les initialisation
+    $moyenne_etd_0 = calculerMoyenne($etudiants[0]['notes']);
+    $somme_moyenne = $moyenne_etd_0;
+    $eleve_max = $etudiants[0];
+    $nom_meilleur = $etudiants[0];
+    $nb_admis = estAdmis($moyenne_etd_0, $seuil) ? 1 : 0;
+
+    for ($i = 1; $i < $taille; $i++) {
+        //la somme des moyennes
+
+        $moyenne_etudiant_courrant = calculerMoyenne($etudiants[$i]["notes"]);
+        $somme_moyenne += $moyenne_etudiant_courrant;
+
+        //meilleur élève
+        if (calculerMoyenne($eleve_max['notes']) < $moyenne_etudiant_courrant) {
+            $eleve_max = $etudiants[$i];
+        }
+
+        //élève le moin bon
+        if (calculerMoyenne($eleve_max['notes']) > $moyenne_etudiant_courrant) {
+            $eleve_min = $etudiants[$i];
+        }
+
+        // nombre admis
+        $nb_admis += estAdmis($moyenne_etudiant_courrant, $seuil) ? 1 : 0;
+
+    }
+    // statistiques de la promotion
+    return array(
+        "moyenne_promo" => round($somme_moyenne / $taille, 1),
+        "meilleur" => formaterNomComplet($eleve_max["nom"], $eleve_max['prenom']),
+        "moin_bon" => formaterNomComplet($eleve_min["nom"], $eleve_min['prenom']),
+        "nb_admis" => $nb_admis,
+        "nb_ajournres" => $taille - $nb_admis,
+        "taux_reussite" => round($nb_admis / $taille * 100, 1)
+    );
 }
